50deac621f012486f00a9a2fc3c0e5b35fb78705
Lukas Mueller <lukas1@umbc.edu>
2020-02-04 02:21:56 -0500

    Simple hello bot

---
+++ unite_bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext.commands import Bot
+from config import token
+
+client = discord.Client()
+
+TOKEN = config.token
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run(TOKEN)
